cc6c4b7c1beb06a0d6cfaebf21ddf4ebd55cb2a8
Ruchi Vivek Desai <ruchi.desai@ufl.edu>
2014-12-23 10:05:00 -0500

    Update setup.py with examples from redi

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from distutils.core import setup
-setup(name='nipype-pbs-workflows',
-      version='1.0',
-      description='Neuroimaging workflows writtten in nipype with a focus on PBS job scheduler',
-      py_modules=['bedpostx', 'dcm2niiconverter'],
-      )
+setup(
+	name='nipype-pbs-workflows',
+    version='1.0',
+    author='https://www.ctsi.ufl.edu/research/study-development/informatics-consulting/',
+    author_email='cts-it-red@ctsi.ufl.edu',
+    description='Neuroimaging workflows writtten in nipype with a focus on PBS job scheduler',
+    long_description=open('README.md').read(),
+    url='https://github.com/ctsit/nipype-pbs-workflows',
+    package_dir={'': 'nipype-pbs-workflows'},
+    packages=[''],
+)
